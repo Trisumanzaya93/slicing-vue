bbd4268b78d00e1c8047fca5db5a15e5e1fafe2d
Trisumanzaya93 <trisumanzaya93a@gmail.com>
2022-08-05 10:14:09 +0700

    page login & detail

---
+++ src/components/atom/button.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <button :class="`btn ${whidth} ${height} ${color} ${border} ${rounded}` ">{{title}}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"ButtonComponent",
+    props:["width","height","color","border","title","rounded"]
+}
+</script>
