9627340e99c017727b80334723a844be15c7eb75
Trisumanzaya93 <trisumanzaya93a@gmail.com>
2022-08-07 12:02:57 +0700

    page vidio belajar

---
+++ src/components/molecule/CardVidio.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div class="card w-72 bg-base-100 shadow-xl">
+  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">
+        Building Backend with NodeJS for Beginner
+      </h2>
+    <p class="truncate">Kunjungi Website Fazztrack Untuk Info Pelatihan https://www.fazztrack.com/Follow juga Akun Instagram Fazztrackhttps://www.instagram.com/fazztrack.id</p>
+    <div class="flex justify-between items-center border-b-2 pb-4 mt-4">
+        <div>
+            <h3 class="text-yellow-400 text-lg">&#9733; &#9733; &#9733; &#9733;</h3>
+        </div>
+        <h3 class="text-sm">Level Basic</h3>
+    </div>
+    <div class="flex justify-between items-center  pb-2 mt-4">
+        <h3 class="text-sm"> Harga Module</h3>        
+        <h3 class="text-sm text-orange-500 font-bold">Gratis</h3>
+    </div>
+    
+  </div>
+</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name :"CardVidio"
+}
+</script>
